624659c2a347ca381a7f7ceee75dc99d4de770c1
Pierre Rioux <pierre.rioux@mcgill.ca>
2018-06-14 18:00:28 -0400

    Removed all meta programming.

--- Ruby/open_science_identity.rb
+++ Ruby/open_science_identity.rb
@@ -3,6 +3,24 @@ require 'i18n'
 require 'active_support/all'
 require 'pbkdf2'
 
+#
+# This class implements a strict hashing function for identifying
+# a patient or subject in a study such that the resulting signature
+# cannot be mapped back to the patient's private information.
+#
+# Usage:
+#
+#   id_obj = OpenScienceIdentity.new(
+#              :gender        => "Male",
+#              :first_name    => "Pierre",
+#              :middle_name   => "Severus",
+#              :last_name     => "Rioux",
+#              :birth_day     => "1902-04-30",
+#              :city_of_birth => "Istanbul, not constantinople",
+#            )
+#   id_obj.validate! # will raise exception if any of the values are not allowed
+#   signature = id_ob.to_signature # PBKDF2 over SHA256
+#
 class OpenScienceIdentity
 
   IDENTITY_ATTRIBUTES        = %i( gender first_name middle_name last_name birth_day city_of_birth )
@@ -14,30 +32,107 @@ class OpenScienceIdentity
   GENDER_VALUES = %w( male female other unknown ) # TODO, adjust based on convention
   DOB_REGEX     = /^(19|20)\d\d-(0[1-9]|1[012])-(0[1-9]|[12]\d|3[01])$/ # TODO, check month lengths?
 
-  attr_accessor *IDENTITY_ATTRIBUTES
-  attr_reader   :bad_attributes
+  # Attribute accessors
+  #
+  # Note: I explicitely avoided using 'attr_accessor' etc so that this
+  # code base could be easily adapted in other OO languages.
 
-  def initialize(attr_hash={})
-    attr_hash.slice(*IDENTITY_ATTRIBUTES).each do |attr_name,value|
-      self.send "#{attr_name}=", value
-    end
+  def gender
+    @gender
   end
 
-  IDENTITY_ATTRIBUTES.each do |attr_name|
-    define_method "clean_#{attr_name}" do
-      plain_alpha(self.send attr_name)
-    end
+  def gender=(val)
+    @gender=val
+  end
+
+  def clean_gender
+    plain_alpha(self.gender)
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def first_name=(val)
+    @first_name=val
+  end
+
+  def clean_first_name
+    plain_alpha(self.first_name)
+  end
+
+  def middle_name
+    @middle_name
+  end
+
+  def middle_name=(val)
+    @middle_name=val
+  end
+
+  def clean_middle_name
+    plain_alpha(self.middle_name)
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def last_name=(val)
+    @last_name=val
+  end
+
+  def clean_last_name
+    plain_alpha(self.last_name)
+  end
+
+  def birth_day
+    @birth_day
+  end
+
+  def birth_day=(val)
+    @birth_day=val
+  end
+
+  def clean_birth_day
+    plain_alpha(self.birth_day)
+  end
+
+  def city_of_birth
+    @city_of_birth
+  end
+
+  def city_of_birth=(val)
+    @city_of_birth=val
+  end
+
+  def clean_city_of_birth
+    plain_alpha(self.city_of_birth)
+  end
+
+  def bad_attributes
+    @bad_attributes
+  end
+
+  def initialize(attr_hash={})
+    self.gender        = attr_hash[:gender]        if attr_hash.has_key?(:gender)
+    self.first_name    = attr_hash[:first_name]    if attr_hash.has_key?(:first_name)
+    self.middle_name   = attr_hash[:middle_name]   if attr_hash.has_key?(:middle_name)
+    self.last_name     = attr_hash[:last_name]     if attr_hash.has_key?(:last_name)
+    self.birth_day     = attr_hash[:birth_day]     if attr_hash.has_key?(:birth_day)
+    self.city_of_birth = attr_hash[:city_of_birth] if attr_hash.has_key?(:city_of_birth)
+    self
   end
 
   def valid?
     @bad_attributes = []
-    IDENTITY_ATTRIBUTES.each do |attr_name|
-      @bad_attributes << attr_name if self.send(attr_name).nil?
-    end
-    @bad_attributes << :gender    if self.gender.present? &&
-                                     (! GENDER_VALUES.include? self.clean_gender)
-    @bad_attributes << :birth_day if self.birth_day.present? &&
-                                     (! DOB_REGEX.match?       self.birth_day)
+    @bad_attributes << :first_name    if self.clean_first_name.blank?
+    @bad_attributes << :middle_name   if self.clean_middle_name.blank?
+    @bad_attributes << :last_name     if self.clean_last_name.blank?
+    @bad_attributes << :gender        if self.gender.blank? ||
+                                         (! GENDER_VALUES.include? self.clean_gender)
+    @bad_attributes << :birth_day     if self.birth_day.blank? ||
+                                         (! DOB_REGEX.match?       self.birth_day)
+    @bad_attributes << :city_of_birth if self.clean_city_of_birth.blank?
     @bad_attributes.size == 0
   end
 
@@ -45,24 +140,27 @@ class OpenScienceIdentity
     raise "Not all identity components have valid initial values: #{@bad_attributes.join(", ")}" unless self.valid?
   end
 
-  def hash_key
-    components = IDENTITY_ATTRIBUTES
-      .map { |attr_name| self.send "clean_#{attr_name}" }
-    components.join('|') # e.g. "male|pierre|tiberius|rioux|1901-01-01|newyorkcity"
+  def signature_key
+    components = [
+      self.clean_gender,
+      self.clean_first_name,
+      self.clean_middle_name,
+      self.clean_last_name,
+      self.clean_birth_day,
+      self.clean_city_of_birth,
+    ]
+    components.join('|') # e.g. "male|pierre|tiberius|rioux|19211231|newyorkcity"
   end
 
   def to_signature
-
     self.validate!
-
-    bar_key  = self.hash_key
-    salt     = PBKDF2_SALT_FUNCTION.call(bar_key)
-
-    pbkdf2               = PBKDF2.new(
-                             :password   => bar_key,
-                             :salt       => salt,
-                             :iterations => PBKDF2_ITERATIONS
-                           )
+    sig_key  = self.signature_key
+    salt     = PBKDF2_SALT_FUNCTION.call(sig_key)
+    pbkdf2   = PBKDF2.new(
+                 :password   => sig_key,
+                 :salt       => salt,
+                 :iterations => PBKDF2_ITERATIONS
+               )
     pbkdf2.hash_function = PBKDF2_HASH_FUNCTION_CLASS.new
     pbkdf2.hex_string
   end
@@ -79,7 +177,7 @@ class OpenScienceIdentity
       raise "Unhandled multibyte characters in string: before: '#{string}' after: '#{cleaned_up}'"
     end
 
-    raise "Empty string produced after cleaning '#{string}'" if cleaned_up.blank?
+    #raise "Empty string produced after cleaning '#{string}'" if cleaned_up.blank?
 
     cleaned_up
   end
--- Ruby/test.rb
+++ Ruby/test.rb
@@ -2,22 +2,33 @@
 
 require './open_science_identity.rb'
 
-report       = ""
-num_sig      = 0
-num_mis      = 0
-num_exc      = 0
-num_inv      = 0
-verbose      = ARGV.size
+report  = ""
+num_sig = 0  ; num_exp_sig = 0
+num_mis = 0
+num_exc = 0  ; num_exp_exc = 0
+num_inv = 0  ; num_exp_inv = 0
+
+verbose = ARGV.size
 
 corrected_db = File.open("new_names_db.csv","w:UTF-8")
 read_db      = File.open("names_db.csv","r:UTF-8")
 
 while line = (read_db.readline rescue nil)
   line.strip!
-  puts "---------------" if verbose > 0
-  puts "LINE=#{line}"    if verbose > 1
+  print "."              if verbose == 1
+  puts "---------------" if verbose > 1
+  puts "LINE=#{line}"    if verbose > 2
   comps = line.split(/\s*,\s*/)
   gender, first, middle, last, dob, city, sig = *comps
+
+  if sig == 'invalid'
+    num_exp_inv += 1
+  elsif sig == 'exception'
+    num_exp_exc += 1
+  else
+    num_exp_sig += 1
+  end
+
   id = OpenScienceIdentity.new(
     :gender        => gender,
     :first_name    => first,
@@ -26,9 +37,10 @@ while line = (read_db.readline rescue nil)
     :birth_day     => dob,
     :city_of_birth => city,
   )
-  puts "KEY=#{(id.hash_key rescue 'exception')}" if verbose > 0
+  puts "KEY=#{(id.signature_key rescue 'exception')}" if verbose > 1
 
   if ! id.valid?
+    puts "  => INVALID, expected: #{sig}" if verbose > 3
     num_inv += 1
     if sig != 'invalid'
       report += "Unexpected invalid: "
@@ -42,6 +54,7 @@ while line = (read_db.readline rescue nil)
   begin
     realsig = id.to_signature
   rescue => ex
+    puts "  => EXCEPTION, expected: #{sig}" if verbose > 3
     num_exc += 1
     if sig != 'exception'
       report += "Unexcepted exception: #{ex.class} #{ex.message} ; Entry=#{line}\n"
@@ -51,9 +64,11 @@ while line = (read_db.readline rescue nil)
   end
 
   if realsig != sig
+    puts "  => SIGNATURE MISMATCH, got #{realsig}, expected: #{sig}" if verbose > 3
     num_mis += 1
     report += "Signature mismatch: Entry=#{line}\n"
   else
+    puts "  => SIGNATURE OK, got #{realsig}" if verbose > 3
     num_sig += 1
   end
   corrected_db.write([gender, first, middle, last, dob, city, realsig].join(",") + "\n")
@@ -63,13 +78,19 @@ end
 read_db.close
 corrected_db.close
 
+puts "\n"
+
 if report.present?
   puts "Report of errors:\n"
+  puts " => #{num_mis} signatures FAILED TO MATCH"
+  puts " => #{num_sig}/#{num_exp_sig} signatures verified"
+  puts " => #{num_exc}/#{num_exp_exc} exceptions"
+  puts " => #{num_inv}/#{num_exp_inv} invalid entries"
   puts report
 else
-  puts "All entries behaved like expected:\n"
-  puts " => #{num_sig} signatures verified"
-  puts " => #{num_exc} exceptions"
-  puts " => #{num_inv} invalid entries"
+  puts "All entries behaved **like expected**:\n"
+  puts " => #{num_sig}/#{num_exp_sig} signatures verified"
+  puts " => #{num_exc}/#{num_exp_exc} exceptions that were expected"
+  puts " => #{num_inv}/#{num_exp_inv} invalid entries that were expected"
 end
 
