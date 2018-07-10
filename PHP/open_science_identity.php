410cdd1b4b00170679a51a48957864b22a7e4252
John Saigle <john.saigle@gmail.com>
2018-07-10 12:30:01 -0400

    Partial implementation

---
+++ PHP/open_science_identity.php
@@ -0,0 +1,146 @@
+<?php
+
+/** This class implements a strict hashing function for identifying
+ *   a patient or subject in a study such that the resulting signature
+ *   cannot be mapped back to the patient's private information.
+ *
+ *   Based on the Ruby implementation by Pierre Rioux.
+ *
+ *   @author John Saigle <john.saigle@mcgill.ca>
+ */
+
+error_reporting(E_ALL);
+
+class OpenScienceIdentity {
+    const IDENTITY_ATTRIBUTES = array(
+        'gender',
+        'first_name',
+        'last_name',
+        'birth_day',
+        'city_of_birth',
+    );
+    const PBKDF2_ITERATIONS = 10000;
+    const PBKDF2_SALT_FUNCTION = 'strrev';
+    const PBKDF2_HASH_FUNCTION = 'sha256';
+
+    // Validations
+    const GENDER_VALUES = array(
+        'male',
+        'female',
+        'unknown',
+        'other'
+    );
+    const DOB_REGEX = '/^(19|20)\d\d-(0[1-9]|1[012])-(0[1-9]|[12]\d|3[01])$/';
+    public $bad_attributes = array();
+
+
+    public function __construct(Array $attributes = []) {
+        $this->gender = $attributes['gender'] ?? '';
+        $this->first_name = $attributes['first_name'] ?? '';
+        $this->middle_name = $attributes['middle_name'] ?? '';
+        $this->last_name = $attributes['last_name'] ?? '';
+        $this->birth_day = $attributes['birth_day'] ?? '';
+        $this->city_of_birth = $attributes['city_of_birth'] ?? '';
+    }
+
+    private function cleanGender(): String {
+        return $this->plainAlpha($this->gender);
+    }
+
+    private function cleanFirstName(): String {
+        return $this->plainAlpha($this->first_name);
+    }
+
+    private function cleanMiddleName(): String {
+        return $this->plainAlpha($this->middle_name);
+    }
+
+    private function cleanLastName(): String {
+        return $this->plainAlpha($this->last_name);
+    }
+    private function cleanBirthDay(): String {
+        return $this->plainAlpha($this->birth_day);
+    }
+    private function cleanCityOfBirth(): String {
+        return $this->plainAlpha($this->city_of_birth);
+    }
+
+    public function valid(): Bool {
+        if (empty($this->cleanFirstName())) {
+            $this->bad_attributes[] = 'first name';
+        }
+        if (empty($this->cleanMiddleName())) {
+            $this->bad_attributes[] = 'middle name';
+        }
+        if (empty($this->cleanLastName())) {
+            $this->bad_attributes[] = 'last name';
+        }
+        if (empty($this->cleanGender()) ||
+            ! in_array($this->cleanGender(), $this::GENDER_VALUES)
+        ) {
+            $this->bad_attributes[] = 'gender';
+        }
+        if (empty($this->birth_day) ||
+            ! preg_match($this::DOB_REGEX, $this->birth_day)
+        ) {
+            $this->bad_attributes[] = 'birthday';
+        }
+        if (empty($this->cleanCityOfBirth())) {
+            $this->bad_attributes[] = 'city of birth';
+        }
+        return count($this->bad_attributes) === 0;
+    }
+
+    function validate() : Void {
+        if (!$this->valid()) {
+            throw new Exception(
+                "Not all identity components have valid initial values: " 
+                . implode(', ', $this->bad_attributes)
+            );
+        }
+    }
+
+    public function signatureKey(): String {
+        $components = array(
+            $this->cleanGender(),
+            $this->cleanFirstName(),
+            $this->cleanMiddleName(),
+            $this->cleanLastName(),
+            $this->cleanBirthDay(),
+            $this->cleanCityOfBirth(),
+        );
+        # e.g. "male|pierre|tiberius|rioux|19211231|newyorkcity"
+        return implode('|', $components);
+    }
+
+    private function plainAlpha($string): String {
+        // Convert $string to a lower-case, anglicized ASCII version of itself
+        setlocale(LC_CTYPE, 'en_US.UTF8');
+        // iconv is here used to change accented characters to unaccented ones
+        //$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+        $cleaned_up = mb_strtolower($string);
+        $cleaned_up = preg_replace('/[^a-z0-9]+/', '', $cleaned_up);
+
+        # TODO: Transliteration check.
+        #if (strlen($string) !== strlen($cleaned_up)) {
+        #    throw(
+        #        new Exception(
+        #            "Unhandled multibyte characters in string: before '$string' after: '$cleaned_up'"
+        #        )
+        #    );
+        #}
+        return $cleaned_up;
+    }
+
+    public function toSignature(): String {
+        $this->validate();
+        $sig_key = $this->signatureKey();
+        $salt = call_user_func($this::PBKDF2_SALT_FUNCTION, $sig_key);
+        return hash_pbkdf2(
+            $this::PBKDF2_HASH_FUNCTION,
+            $sig_key, 
+            $salt, 
+            $this::PBKDF2_ITERATIONS
+        );
+    }
+}
---
+++ PHP/test.php
@@ -0,0 +1,75 @@
+<?php
+
+error_reporting(E_ALL);
+// If experiencing errors with encoding, uncomment this line and verify the 
+// encodings are set to UTF-8.
+//var_dump(iconv_get_encoding('all'));
+
+require('open_science_identity.php');
+
+$report  = "";
+$num_sig = $num_exp_sig = 0;
+$num_mis = 0;
+$num_exc = $num_exp_exc = 0;
+$num_inv = $num_exp_inv = 0;
+
+// TODO: move the test db from the Ruby folder
+// Open the CSV database.  Iterate over each line and store the values in the
+// array $line.  Extract these values into individual variables.
+$db_filepath = "../Ruby/names_db.csv";
+if (($handle = fopen($db_filepath, "r")) !== FALSE) {
+    while (($line = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        list($gender, $first_name, $middle_name, $last_name, $birth_day, $city_of_birth, $sig) = $line;
+        // Keep track of totals
+        if ($sig === 'invalid') {
+            $num_exp_inv += 1;
+        } else if ($sig === 'exception') {
+            $num_exp_exc += 1;
+        } else {
+            $num_exp_sig += 1;
+        }
+
+        $id = new OpenScienceIdentity(
+          compact(
+              "gender",
+              "first_name",
+              "middle_name",
+              "last_name",
+              "birth_day",
+              "city_of_birth"
+          )
+        );
+        echo "KEY={$id->signatureKey()}\n";
+
+        if (! $id->valid()) {
+            // TODO: add verbosity levels.
+            echo " => INVALID. expected $sig\n";
+            $num_inv += 1;
+            if (! $sig === 'invalid') {
+                $report .= "Unexpected invalid: ";
+                $report .= implode(', ', $id->bad_attributes);
+            }
+        }
+
+        try {
+            $realsig = $id->toSignature();
+        } catch (Exception $e) {
+            echo "  => EXCEPTION, expected: {$sig}";
+            $num_exc += 1;
+            if ($sig !== 'exception') {
+                $report .= "Unexcepted exception: {$e->getMessage()}";
+            }
+        }
+
+        if ($realsig !== $sig) {
+            echo "  => SIGNATURE MISMATCH, got {$realsig}, expected: {$sig}\n";
+            $num_mis += 1;
+            $report .= "Signature mismatch: Entry=#{line}\n";
+        } else {
+            echo " => SIGNATURE OK, got {$realsig}\n";
+            $num_sig += 1;
+        }
+    }
+    fclose($handle);
+}
+
